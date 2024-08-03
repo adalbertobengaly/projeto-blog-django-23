1c41d425b5638eb0844a5ec77a2c2bc141057b03
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-02 21:46:25 -0300

    Migrando a view index para Class Based View django.views.generic.list.ListView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,29 +1,53 @@
 # type: ignore
+from typing import Any
 from django.core.paginator import Paginator
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from blog.models import Page, Post
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.http import Http404
+from django.views.generic import ListView
 
 PER_PAGE = 9
 
+class PostLitView(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'posts'
+    ordering = "-pk"
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published()
 
-def index(request):
-    posts = Post.objects.get_published()  
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+        context.update({
             'page_title': 'Home - ',
-        }
-    )
+        })
+
+        return context
+
+# def index(request):
+    # posts = Post.objects.get_published()  
+
+    # paginator = Paginator(posts, PER_PAGE)
+    # page_number = request.GET.get("page")
+    # page_obj = paginator.get_page(page_number)
+
+    # return render(
+    #     request,
+    #     'blog/pages/index.html',
+    #     {
+    #         'page_obj': page_obj,
+    #         'page_title': 'Home - ',
+    #     }
+    # )
 
 
 def created_by(request, author_pk):

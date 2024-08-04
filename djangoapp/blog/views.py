1c579ccf2ad3611381ca3088ec3294a1593b79e9
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-04 12:01:42 -0300

    Migrando a view category para ListView e usando allow_empty para 404

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -32,36 +32,6 @@ class PostListView(ListView):
         return context
 
 
-def created_by(request, author_pk):
-    user = User.objects.filter(pk=author_pk).first()
-
-    if user is None:
-        raise Http404()
-
-    posts = (
-        Post.objects.get_published()
-        .filter(created_by__pk=author_pk)
-    )
-
-    user_full_name = user.username
-
-    if user.first_name:
-        user_full_name = f'{user.first_name} {user.last_name}'
-    page_title = 'Posts de ' + user_full_name + ' - '
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
-
 class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
@@ -100,6 +70,24 @@ class CreatedByListView(PostListView):
         })
 
         return super().get(request, *args, **kwargs)
+
+
+class CategoryListView(PostListView):
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page_title = f'{self.object_list[0].category.name} - Categoria - ' 
+        ctx.update({
+            'page_title': page_title
+        })
+        return ctx
+
 
 
 def category(request, slug):
